1b1a7ef9389f0d2b93b41e04d98eae6b3ca987a3
WKD622 <kuba.ziarko@gmail.com>
2020-04-20 18:05:08 +0200

    c

--- bucket_sort.c
+++ bucket_sort.c
@@ -1,12 +1,17 @@
 #include <omp.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+
 
 int main(int argc, char **argv) {
+    srand(time(0)); 
     const int N = 100000;
     int i, a[N];
 
     #pragma omp parallel for
     for (i = 0; i < N; i++)
-        a[i] = 2 * i;
+        a[i] = rand() % 100;
 
     return 0;
  }
